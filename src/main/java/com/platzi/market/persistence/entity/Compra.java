2debfa5901606a68ad0005e9e4fd8bf8acc15e5b
Juan Lara <max_lara3005@hotmail.com>
2023-06-20 21:59:01 -0600

    Exponiendo la primera api

--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -23,7 +23,23 @@ public class Compra {
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "producto")
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ComprasProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ComprasProducto> productos) {
+        this.productos = productos;
+    }
+
+    @OneToMany(mappedBy = "compra")
     private List<ComprasProducto> productos;
 
     public Integer getIdCompra() {
